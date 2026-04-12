e4c7d177b5220a8b1c01a5b87c0beaf1a2856785
amoakuelliotenam <amoakuelliotenam@gmail.com>
2026-04-12 11:05:31 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const e = require("express");
+
+const app = express();
+app.use(cors());
